a699ffb59855625a2418cd5418c83c9198da09a5
yuliya <yulyakam85@mail.ru>
2023-09-22 14:58:21 +0300

    Add solution Homework6

---
+++ HW6/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// /**
+//  * 1) Дан массив const arr = [1, 5, 7, 9] с помощью Math.min и spread оператора,
+// найти минимальное число в массиве, решение задание должно состоять из одной
+// строки кода.
+//  */
+// // const arr = [1, 5, 7, 9];
+// // console.log(Math.min(...arr));
+
+// // Решение в одну строку кода:
+// // console.log(Math.min(...[1, 5, 7, 9]));
+
+// /**
+//  * 2) Напишите функцию createCounter, которая создает счетчик и возвращает объект
+// с двумя методами: increment и decrement. Метод increment должен увеличивать
+// значение счетчика на 1, а метод decrement должен уменьшать значение счетчика
+// на 1. Значение счетчика должно быть доступно только через методы объекта,
+// а не напрямую.
+//  */
+// function createCounter() {
+//   let count = 0;
+//   return {
+//     increment: function () {
+//       return count++;
+//     },
+//     decrement: function () {
+//       return count--;
+//     },
+//   };
+// }
+// const counter = createCounter();
+// // console.log(counter.increment());
+// // console.log(counter.increment());
+// // console.log(counter.increment());
+// // console.log(counter.decrement());
+// // console.log(counter.decrement());
+
+// // console.log(counter.count);// undefined т.к Значение счетчика доступно только через методы объекта
+
+// /**
+//  *
+//  */
+
+
+
+
