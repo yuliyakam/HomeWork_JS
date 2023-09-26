86266b759d1992b9320f92e8d5b76b8f2d38a87f
yuliya <yulyakam85@mail.ru>
2023-09-26 13:57:37 +0300

    Add solution Homework8

---
+++ HW8/script.js
@@ -0,0 +1,102 @@
+/**
+ * Задание 1: "Управление персоналом компании"
+
+Реализуйте класс Employee (сотрудник), который имеет следующие свойства и методы:
+Свойство name (имя) - строка, имя сотрудника.
+Метод displayInfo() - выводит информацию о сотруднике (имя) в консоль.
+
+Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
+Свойство department (отдел) - строка, отдел, в котором работает менеджер.
+Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел).
+
+// Пример использования классов
+const employee = new Employee("John Smith");
+employee.displayInfo(); // "Name: John Smith"
+
+const manager = new Manager("Jane Doe", "Sales");
+manager.displayInfo(); // "Name: John Doe, Department: Sales"
+ */
+
+class Employee {
+  constructor(name) {
+    this.name = name;
+  }
+  displayInfo() {
+    return `Name: ${this.name}`;
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, department) {
+    super(name);
+    this.department = department;
+  }
+  displayInfo() {
+    return `${super.displayInfo()}, Department: ${this.department}`;
+  }
+}
+
+const employee = new Employee("John Smith");
+console.log(employee.displayInfo());
+
+const manager = new Manager("Jane Doe", "Sales");
+console.log(manager.displayInfo());
+
+/**
+  * Задание 2: "Управление списком заказов"
+
+Реализуйте класс Product (товар), который имеет следующие свойства и методы:
+Свойство name - название товара.
+Свойство price - цена товара.
+Свойство quantity - количество товара.
+
+Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+Свойство id (номер заказа) - число, уникальный номер заказа.
+Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+Метод addProduct(product) - принимает объект класса Product и добавляет его в список продуктов заказа.
+Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+// Пример использования:
+const order = new Order(12345);
+
+const product1 = new Product("Phone", 500, 2);
+order.addProduct(product1);
+
+const product2 = new Product("Headphones", 100, 1);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice()); // Вывод: 1100
+  */
+
+class Product {
+  constructor(name, price, quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+}
+class Order {
+  products = [];
+  constructor(orderNumber) {
+    this.orderNumber = orderNumber;
+  }
+  addProduct(product) {
+    this.products.push(product);
+  }
+  getTotalPrice() {
+    return this.products.reduce(
+      (sum, el) => (sum += el.price * el.quantity),
+      0
+    );
+  }
+}
+
+const order = new Order(12345);
+
+const product1 = new Product("Phone", 500, 2);
+order.addProduct(product1);
+
+const product2 = new Product("Headphones", 100, 1);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice()); 
